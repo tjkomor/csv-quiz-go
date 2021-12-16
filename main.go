c221d45f67a5a7228a7350f7905c345eb3581f1b
tyler komoroske <tjkomor@clemson.edu>
2021-12-16 10:25:01 -0700

    read csv file and parse lines to print out the question, implement read answer from file next

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", " a csv file in the format of question,answer")
+
+	flag.Parse()
+	
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("failed to open the csv file %s\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("failed to parse provided CSV file")
+	}
+	
+	problems := parseLines(lines)
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+	}
+}
+
+type problem struct {
+	question string
+	answer string 
+}
+
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer: line[1],
+		}
+	}
+	return problems 
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
